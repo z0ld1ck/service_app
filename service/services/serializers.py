51ca34c2382884d8f42b4f82891158c9bd8e2761
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2023-05-23 22:02:54 +0600

    Day4  - 1

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -6,7 +6,13 @@ from services.models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return (instance.service.full_price
+                - instance.service.full_price * (instance.plan.discount_percent / 100))
+
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'price')
--- service/services/views.py
+++ service/services/views.py
@@ -1,8 +1,18 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from clients.models import Client
 from services.models import Subscription
+from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.all().prefetch_related(
+        'plaxn',
+        Prefetch('client',
+                 queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                           'user__email'))
+    )
+    serializer_class = SubscriptionSerializer
+    
